fe4d711b37e8d4c2fe9aa473dcb3e2801ff964cc
Scott Semyan <scottse@microsoft.com>
2020-12-04 13:11:59 -0800

    Day 3 pt two

--- day03.go
+++ day03.go
@@ -18,14 +18,30 @@ func main() {
 		for col, square := range line {
 			if square == '#' {
 				key := strconv.Itoa(row) + "_" + strconv.Itoa(col)
-				fmt.Println("Found tree: ", key)
+				//fmt.Println("Found tree: ", key)
 				trees[key] = true
 			}
 		}
 	}
 
-	// Now traverse down
-	row, col, rowMv, colMv, treesHit := 0, 0, 1, 3, 0
+	// Right 1, down 1.
+	// Right 3, down 1. (This is the slope you already checked.)
+	// Right 5, down 1.
+	// Right 7, down 1.
+	// Right 1, down 2.
+
+	treesHit := findTrees(1, 1, numCols, numRows, trees)
+	treesHit *= findTrees(3, 1, numCols, numRows, trees)
+	treesHit *= findTrees(5, 1, numCols, numRows, trees)
+	treesHit *= findTrees(7, 1, numCols, numRows, trees)
+	treesHit *= findTrees(1, 2, numCols, numRows, trees)
+
+	fmt.Printf("Hit %d total trees\n", treesHit)
+}
+
+func findTrees(colMv int, rowMv int, numCols int, numRows int, trees map[string]bool) int {
+
+	row, col, treesHit := 0, 0, 0
 	for {
 		row += rowMv
 		col += colMv
@@ -36,12 +52,14 @@ func main() {
 			break
 		}
 		key := strconv.Itoa(row) + "_" + strconv.Itoa(col)
-		fmt.Println("Checking ", key)
+		//fmt.Println("Checking ", key)
 		_, exists := trees[key]
 		if exists {
 			treesHit++
-			fmt.Println("Hit tree")
+			//fmt.Println("Hit tree")
 		}
 	}
 	fmt.Printf("Hit %d trees\n", treesHit)
+
+	return treesHit
 }
