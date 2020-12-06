82b84c1ff8d1bbfb1772f8becf402949355f7454
Scott Semyan <scottse@microsoft.com>
2020-12-06 11:37:18 -0800

    Day 4 part two

--- day04/day04.go
+++ day04/day04.go
@@ -3,6 +3,8 @@ package main
 import (
 	"AdventOfCode2020/mods/fileload"
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -16,7 +18,6 @@ func main() {
 	fmt.Printf("Found %d recs\n", len(recs))
 
 	// Turn each record into map
-	//passports := make([]map[string]string, len(recs))
 	validPass := 0
 	for _, rec := range recs {
 		recData := strings.Split(strings.ReplaceAll(rec, "\n", " "), " ")
@@ -28,7 +29,6 @@ func main() {
 		if IsValidPassport(passport) {
 			validPass++
 		}
-		//passports[i] = passport
 	}
 
 	fmt.Println("Valid passports: ", validPass)
@@ -39,22 +39,76 @@ func main() {
 func IsValidPassport(passport map[string]string) bool {
 
 	// Must have all but cid
-	// byr (Birth Year)
-	// iyr (Issue Year)
-	// eyr (Expiration Year)
-	// hgt (Height)
-	// hcl (Hair Color)
-	// ecl (Eye Color)
-	// pid (Passport ID)
-	// cid (Country ID)
+	// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+	// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+	// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+	// hgt (Height) - a number followed by either cm or in:
+	// If cm, the number must be at least 150 and at most 193.
+	// If in, the number must be at least 59 and at most 76.
+	// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+	// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+	// pid (Passport ID) - a nine-digit number, including leading zeroes.
+	// cid (Country ID) - ignored, missing or not.
 
 	fields := [7]string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+	hgtTest := regexp.MustCompile("^(\\d+)([ic][nm])$")
+	hclTest := regexp.MustCompile("^#[0-9a-f]{6}$")
+	eclTest := regexp.MustCompile("^amb|blu|brn|gry|grn|hzl|oth$")
+	pidTest := regexp.MustCompile("^[0-9]{9}$")
+
 	for _, field := range fields {
-		_, exists := passport[field]
+		value, exists := passport[field]
 		if !exists {
-			fmt.Println("Missing field: ", field)
+			//fmt.Println("Missing field: ", field)
+			return false
+		}
+
+		// validate fields
+		passportGood := true
+		switch field {
+		case "byr":
+			n, err := strconv.Atoi(value)
+			if err != nil || (n < 1920 || n > 2002) {
+				passportGood = false
+			}
+
+		case "iyr":
+			n, err := strconv.Atoi(value)
+			if err != nil || (n < 2010 || n > 2020) {
+				passportGood = false
+			}
+
+		case "eyr":
+			n, err := strconv.Atoi(value)
+			if err != nil || (n < 2020 || n > 2030) {
+				passportGood = false
+			}
+
+		case "hgt":
+			parts := hgtTest.FindStringSubmatch(value)
+			passportGood = (len(parts) == 3)
+			if passportGood {
+				n, err := strconv.Atoi(parts[1])
+				if err != nil || (parts[2] == "in" && (n < 59 || n > 76)) || (parts[2] == "cm" && (n < 150 || n > 193)) {
+					passportGood = false
+				}
+			}
+
+		case "hcl":
+			passportGood = hclTest.MatchString(value)
+
+		case "ecl":
+			passportGood = eclTest.MatchString(value)
+
+		case "pid":
+			passportGood = pidTest.MatchString(value)
+		}
+
+		if !passportGood {
+			fmt.Printf("%s invalid: %s\n", field, value)
 			return false
 		}
 	}
+
 	return true
 }
