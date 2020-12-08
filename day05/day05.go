5c2cb10ccfdf809cb8a83390fc1d3d9e637ff4cc
Scott Semyan <scottse@microsoft.com>
2020-12-07 16:59:37 -0800

    Day 5 part two

--- day05/day05.go
+++ day05/day05.go
@@ -12,7 +12,7 @@ func main() {
 	lines := fileload.Fileload("day05/data.txt")
 
 	maxSeatCode := 0.0
-
+	codeList := make(map[string]bool)
 	for _, line := range lines {
 
 		rowMin, rowMax := 0.0, 127.0
@@ -41,11 +41,28 @@ func main() {
 			panic("Wrong data")
 		}
 
+		// Determine max code
 		seatCode := rowMin*8 + colMin
 		if seatCode > maxSeatCode {
 			maxSeatCode = seatCode
 		}
+
+		// Add to list
+		key := fmt.Sprintf("%g_%g", rowMin, colMin)
+		codeList[key] = true
+
 		fmt.Println("Result: ", line, rowMin, rowMax, colMin, colMax, seatCode)
 	}
 	fmt.Println("Max seat code: ", maxSeatCode)
+
+	// Find seat (part two)
+	for row := 1; row < 127; row++ {
+		for col := 0; col < 8; col++ {
+			key := fmt.Sprintf("%d_%d", row, col)
+			_, exists := codeList[key]
+			if !exists {
+				fmt.Println("Missing seat at ", key, row*8+col)
+			}
+		}
+	}
 }
