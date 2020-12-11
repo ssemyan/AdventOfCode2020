53827fb2c142c244a0c169703cf49dfe90542927
Scott Semyan <scottse@microsoft.com>
2020-12-11 14:22:01 -0800

    Day 9 part two

--- day09/day09.go
+++ day09/day09.go
@@ -13,6 +13,7 @@ func main() {
 	// Part One
 	preamble := 25
 	nCurrent := preamble
+	invalidNum := 0
 
 	for {
 		if nCurrent >= len(lines) {
@@ -36,8 +37,54 @@ func main() {
 		}
 		if !bFoundCombo {
 			fmt.Println("No combo found line ", nCurrent, lines[nCurrent])
+			invalidNum = lines[nCurrent]
 			break
 		}
 		nCurrent++
 	}
+
+	// part two
+	nMin, nMax := 0, 0
+	nCurrent = 0
+	for {
+		if nCurrent >= len(lines) {
+			break
+		}
+
+		nStart := nCurrent
+		nEnd := len(lines)
+		bFoundCombo := false
+		for i := nStart; i < nEnd; i++ {
+			nSum := lines[i]
+			for z := i + 1; z < nEnd; z++ {
+				nSum += lines[z]
+				if invalidNum == nSum {
+					fmt.Println("Found contiguous nums: ", i, z)
+					nMin = lines[i] // give it something other than 0
+					for y := i; y <= z; y++ {
+						if lines[y] < nMin {
+							nMin = lines[y]
+						}
+						if lines[y] > nMax {
+							nMax = lines[y]
+						}
+					}
+					bFoundCombo = true
+					break
+				}
+				if nSum > invalidNum {
+					break
+				}
+			}
+			if bFoundCombo {
+				break
+			}
+		}
+		if bFoundCombo {
+			fmt.Println("Found lines ", nMin, nMax, (nMin + nMax))
+			break
+		}
+		nCurrent++
+	}
+
 }
