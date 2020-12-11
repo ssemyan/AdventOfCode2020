0e538258cb1a9b7b36fa06e16822eb80d3336c00
Scott Semyan <scottse@microsoft.com>
2020-12-11 12:49:47 -0800

    Day 8 part two

--- day08/day08.go
+++ day08/day08.go
@@ -7,12 +7,34 @@ import (
 	"strings"
 )
 
+type RunResult struct {
+	accumulator int
+	infinite    bool
+}
+
 func main() {
 
 	// Read all data into memory
 	lines := fileload.Fileload("day08/data.txt")
 
+	// Part One
+	result := testCode(lines, -1)
+	fmt.Printf("Result acc: %v inf: %v \n", result.accumulator, result.infinite)
+
+	// Part Two
+	for i := range lines {
+		result = testCode(lines, i)
+		if !result.infinite {
+			fmt.Printf("Found wrong line %v add %v", i, result.accumulator)
+			break
+		}
+	}
+}
+
+func testCode(lines []string, rowToTry int) RunResult {
+
 	accumulator := 0
+	infinite := false
 
 	// keep track of where we have been
 	ran := make([]bool, len(lines))
@@ -20,8 +42,15 @@ func main() {
 	// run program
 	curIns := 0
 	for {
+		if curIns == len(lines) {
+			// program successfully ran
+			break
+		}
+
 		curLine := lines[curIns]
 		if ran[curIns] {
+			// infinite loop detected
+			infinite = true
 			break
 		}
 		ran[curIns] = true
@@ -29,7 +58,18 @@ func main() {
 		fmt.Println(curLine)
 		parts := strings.Split(curLine, " ")
 		n, _ := strconv.Atoi(parts[1])
-		switch parts[0] {
+
+		// Swap instruction if indicated
+		inst := parts[0]
+		if curIns == rowToTry {
+			if inst == "jmp" {
+				inst = "nop"
+			} else if inst == "nop" {
+				inst = "jmp"
+			}
+		}
+
+		switch inst {
 		case "acc":
 			accumulator += n
 			curIns++
@@ -38,9 +78,12 @@ func main() {
 		case "nop":
 			curIns++
 		default:
-			panic(fmt.Sprintf("Unknown command %s", parts[0]))
+			panic(fmt.Sprintf("Unknown command %s", inst))
 		}
 	}
 
-	fmt.Println("Acc ", accumulator)
+	return RunResult{
+		accumulator: accumulator,
+		infinite:    infinite,
+	}
 }
