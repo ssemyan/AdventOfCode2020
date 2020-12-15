262b2cb847c45033543f9f3bc1720adbb0c03d29
Scott Semyan <scottse@microsoft.com>
2020-12-15 14:25:42 -0800

    Day 10 part two

--- day10/day10.go
+++ day10/day10.go
@@ -32,6 +32,53 @@ func main() {
 	// Last is always 3 away
 	nThree++
 
-	fmt.Printf("Results: 1=%d 3=%d final=%d ", nOne, nThree, nOne*nThree)
+	fmt.Printf("Results: 1=%d 3=%d final=%d \n", nOne, nThree, nOne*nThree)
 
+	// Part Two
+	//nCombos := 1
+	foundCombos := make(map[int]int)
+	nCombos := FindNextAdaptor(lines, 0, 0, foundCombos)
+
+	fmt.Println("Total Combos: ", nCombos+1)
+
+}
+
+// FindNextAdaptor - find and count the next adapter
+func FindNextAdaptor(lines []int, nCurrVolts int, lineIndex int, foundCombos map[int]int) int {
+
+	// See if we know how many combos from here
+	val, exists := foundCombos[nCurrVolts]
+	if exists {
+		//fmt.Println("found ", val)
+		return val
+	}
+
+	bOnFirst := true
+	nCombos := 0
+	for {
+		// Check if at end
+		if lineIndex >= len(lines) {
+			break
+		}
+
+		// Is the next match in range
+		if (lines[lineIndex] - nCurrVolts) < 4 {
+			//fmt.Printf(" %d", lines[lineIndex])
+
+			// is it the first match
+			if bOnFirst {
+				bOnFirst = false
+			} else {
+				nCombos++
+				//fmt.Printf(" break")
+			}
+
+			nCombos += FindNextAdaptor(lines, lines[lineIndex], lineIndex+1, foundCombos)
+		}
+		lineIndex++
+	}
+
+	// Save combo
+	foundCombos[nCurrVolts] = nCombos
+	return nCombos
 }
