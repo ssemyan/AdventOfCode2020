6dfb04dd94b514b68f95c90237c28e182006bb8e
Scott Semyan <scottse@microsoft.com>
2020-12-26 18:30:51 -0800

    Day 16 part two

--- day16/day16.go
+++ day16/day16.go
@@ -24,9 +24,10 @@ func main() {
 	sumInvalid := 0
 
 	rules := make(map[int]rule)
+	validRules := make(map[int]map[int]bool)
 	stage := "rules"
 	myTicket := ""
-	rulerex := regexp.MustCompile("^([a-zA-Z ])+: (\\d+)-(\\d+) or (\\d+)-(\\d+)$")
+	rulerex := regexp.MustCompile("^([a-zA-Z ]+): (\\d+)-(\\d+) or (\\d+)-(\\d+)$")
 	for _, line := range lines {
 
 		if stage == "rules" {
@@ -64,11 +65,13 @@ func main() {
 			// Test each ticket
 			// Get numbers from each line
 			numsa := strings.Split(line, ",")
+			badTicket := false
 			for _, num := range numsa {
 				v, _ := strconv.Atoi(num)
 
 				bBadNum := true
 				for _, r := range rules {
+					// Does number pass at least one rule?
 					if (v >= r.lowMin && v <= r.lowMax) || (v >= r.hiMin && v <= r.hiMax) {
 						bBadNum = false
 					}
@@ -76,10 +79,85 @@ func main() {
 				if bBadNum {
 					fmt.Println("Bad num: ", v)
 					sumInvalid += v
+					badTicket = true
+				}
+			}
+
+			if !badTicket {
+
+				// Create list of potential rules for each position
+				for pos, num := range numsa {
+
+					v, _ := strconv.Atoi(num)
+
+					potentialRules := make(map[int]bool)
+					for ruleNum, r := range rules {
+						// which rules pass for this position?
+						if (v >= r.lowMin && v <= r.lowMax) || (v >= r.hiMin && v <= r.hiMax) {
+							potentialRules[ruleNum] = true
+						}
+					}
+
+					posPotentialRules, exists := validRules[pos]
+					if !exists {
+						// First time finding potential rules for this pos
+						validRules[pos] = potentialRules
+					} else {
+						// Remove any rules that are not in the join of the two sets
+						commonRules := make(map[int]bool)
+						for ruleNum := range posPotentialRules {
+							_, exists := potentialRules[ruleNum]
+							if exists {
+								commonRules[ruleNum] = true
+							}
+						}
+						validRules[pos] = commonRules
+					}
 				}
 			}
 		}
 	}
 
+	// Narrow down potential rules
+	for {
+
+		bFoundMulti := false
+		for pos, posRules := range validRules {
+			if len(posRules) == 1 {
+				// since this pos has only one possible rule, remove that rule from all other potentials
+				for ruleNum := range posRules {
+					for poso, posRuleso := range validRules {
+						if pos != poso {
+							_, exists := posRuleso[ruleNum]
+							if exists {
+								delete(posRuleso, ruleNum)
+								bFoundMulti = true
+							}
+						}
+					}
+				}
+			}
+		}
+		if !bFoundMulti {
+			break
+		}
+	}
+
+	// Print rules for each position
+	myTicketNums := strings.Split(myTicket, ",")
+	deptVal := 1
+	for pos, posRules := range validRules {
+		fmt.Printf("Pos: %d Rule: ", pos)
+		for ruleNum := range posRules {
+			fmt.Printf("%d %s \n", ruleNum, rules[ruleNum].name)
+			if strings.HasPrefix(rules[ruleNum].name, "departure") {
+				posVal, _ := strconv.Atoi(myTicketNums[pos])
+				deptVal *= posVal
+			}
+		}
+	}
+
 	fmt.Println("Sum invalid: ", sumInvalid)
+
+	fmt.Println("Sum my ticket departures: ", deptVal)
 }
